6d2cae21ea16f14fa2efec9188d7366cf86c4946
MyTHOMASTHOMAS <thomasthomasz2xz@gmail.com>
2025-11-28 16:05:38 +0400

    FEAT(PWA): Ajout du pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AppProvider } from './contexts/AppContext'
 import './styles/index.css'
+import { registerSW } from 'virtual:pwa-register'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,3 +12,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </AppProvider>
   </React.StrictMode>,
 )
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        // Appelé quand une nouvelle version est disponible (si vous n'utilisez pas autoUpdate)
+        if (confirm("Nouvelle version disponible. Recharger ?")) {
+            updateSW(true)
+        }
+    },
+    onOfflineReady() {
+        console.log("App prête à fonctionner hors-ligne !")
+    },
+})
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,55 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico'],
+
+            // --- MANIFEST (Identité de l'app) ---
+            // Je laisse cette partie car elle est obligatoire pour l'installation
+            manifest: {
+                name: 'RecipGen',
+                short_name: 'GenCipe',
+                description: 'Générez des recettes intelligentes avec vos ingrédients',
+                theme_color: '#10b981',
+                background_color: '#f8fafc',
+                display: 'standalone',
+                orientation: 'portrait',
+                icons: [
+                    {
+                        src: '/icon/64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icon/144x144.png',
+                        sizes: '144x144',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icon/192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        purpose: 'any'
+                    },
+                    {
+                        src: '/icon/512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ]
+            },
+
+            // --- MODE DEV ---
+            devOptions: {
+                enabled: true,
+                type: 'module',
+            }
+        })
+    ],
 })
